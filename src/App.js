d6a54659186b5fb2ae24073c878a6305a1228b39
Andrew-R-Lawler <61293731+Andrew-R-Lawler@users.noreply.github.com>
2022-09-07 19:14:42 -0500

    products component added to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Products from './components/Products';
 
 const App = () => {
   return (
-    <div>E-commerce</div>
+    <div>
+        <Products />
+    </div>
   )
 }
 
