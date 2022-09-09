7d9b329f426c3a2edb9f49005376d2a2f3c8f145
Andrew-R-Lawler <61293731+Andrew-R-Lawler@users.noreply.github.com>
2022-09-09 16:38:46 -0500

    commerce.js imported and product list array call successful

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
-import Product from './Product';
+import Product from './Product/Product';
 import macbook from '../../assets/macbook.jpg';
 import shoes from '../../assets/shoes.jpg';
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Running Shoes.', price: '$5', image: macbook },
@@ -11,8 +12,11 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
