71ea8354b3522c04030d8c84ebd9fe745b8241bf
Andrew-R-Lawler <61293731+Andrew-R-Lawler@users.noreply.github.com>
2022-09-10 02:19:04 -0500

    final commit of the day

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const App = () => {
     const [cart, setCart] = useState({});
     const [products, setProducts] = useState ([]);
+    const [order, setOrder] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -34,6 +36,20 @@ const App = () => {
         const { cart } = await commerce.cart.empty();
     }
 
+    const refreshCart = async () => {
+        const newCart = await commerce.cart.refresh();
+        setCart(newCart);
+    }
+
+    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+        try {
+            const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+            setOrder(incomingOrder);
+        } catch (error) {
+            setErrorMessage(error.data.error.message);
+        }
+    }
+
     useEffect(() => {
         fetchCart();
         fetchProducts();
@@ -46,7 +62,7 @@ const App = () => {
             <Routes>
                 <Route path='/' element={<Products products={products} onAddToCart={handleAddToCart}/>} />
                 <Route path='/cart' element={<Cart cart={cart} handleUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart} handleEmptyCart={handleEmptyCart} />} />
-                <Route path='/checkout' element={<Checkout cart={cart} />}/>
+                <Route path='/checkout' element={<Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />}/>
             </Routes>
         </div>
     </Router>
